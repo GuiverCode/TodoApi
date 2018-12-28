f59c2081643abcbf901d152b1075fdf60c1443a3
guille <guille@localhost.localdomain>
2018-12-28 19:15:47 -0300

    lista y crud de usuarios

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,16 +1,24 @@
 using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
     public class Usuario: Persona
     {
+        public const int UsernameMaxLength = 64;
+        public const int PasswordMaxLength = 5;
+
+
         public long Id{get; set;}
 
         [Required]
+        [MaxLength(UsernameMaxLength)]
         public string Username {get; set;}
         [Required]
-        public string Password {get; set;}
+        [JsonIgnore]
+        public byte[] HashPassword { get; set; }
+
         
     }
 }
